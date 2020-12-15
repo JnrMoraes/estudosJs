f49325bfb9d363210e5df22851c2134204d2614e
juniormoraes <contatomoraesjr@gmail.com>
2020-12-15 15:30:43 -0300

    last edit

--- exerciciosJs/esnext/async_await/async_await_2.js
+++ exerciciosJs/esnext/async_await/async_await_2.js
@@ -1,35 +1,33 @@
-function gerarNumerosEntre(min, max, numerosProibidos){
-    if (min > max) [max, min] = [min, max]
-    return new Promise((resolve,reject) => {
-        const fator = max - min +1
-        const aleatorio = parseInt(Math.random() * fator ) + min
-        if (numerosProibidos.includes(aleatorio)) {
-            reject('Número repetido!')
-        } else {
-            resolve(aleatorio)
-        }
-    })
+function gerarNumerosEntre(min, max, numerosProibidos) {
+  if (min > max) [max, min] = [min, max];
+  return new Promise((resolve, reject) => {
+    const fator = max - min + 1;
+    const aleatorio = parseInt(Math.random() * fator) + min;
+    if (numerosProibidos.includes(aleatorio)) {
+      reject("Número repetido!");
+    } else {
+      resolve(aleatorio);
+    }
+  });
 }
 
 async function gerarMegaSena(qtDeNumeros, tentativas = 1) {
-    try {
-        const numeros = []
+  try {
+    const numeros = [];
     for (let _ of Array(qtDeNumeros).fill()) {
-        numeros.push(await gerarNumerosEntre(1,60,numeros))
+      numeros.push(await gerarNumerosEntre(1, 60, numeros));
     }
-    return numeros
-    } catch (error) {
-        if(tentativas > 10){
-            throw ' Que chato!!!'
-        } else {
-            return gerarMegaSena(qtDeNumeros, tentativas +1)
-        }
+    return numeros;
+  } catch (error) {
+    if (tentativas > 10) {
+      throw " Que chato!!!";
+    } else {
+      return gerarMegaSena(qtDeNumeros, tentativas + 1);
     }
+  }
 }
 
-gerarMegaSena(15)
-    .then(console.log)
-    .catch(console.log)
+gerarMegaSena(15).then(console.log).catch(console.log);
 
 // gerarNumerosEntre(1,5, [1,2,4])
 //     .then(console.log)
